200003ce21af7737f1443f33c8dee07a79fe5226
Owen Urkov <ourkov@gopro.com>
2017-01-18 16:56:56 -0800

    Make this a web app.  Update README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # wsdk_version_bumper
 Bumps minor or point version of Android WSDK.  
+
+Need more thorough steps on how this works
+
+/bumppoint/<branch>/<ticket>
+would be used on a release branch after a release goes out.  Something like:
+
+bumppoint/wsdk0.15/CAL-1234
+
+would do the following:
+checkout release/wsdk0.15
+create/checkout feature/CAL-1234 branch from the release branch
+bump the point version from, say, 0.15.0 to 0.15.1 
+add/commit the change
+push feature/CAL-1234 to origin
+
+
+/bumpminor/develop/CAL-4567
+
+would do the following:
+checkout develop
+create/checkout feature/CAL-4567 branch from develop
+bump the minor version from, say, 0.15.0 to 0.16.0
+add/commit the changes
+push feature/CAL-4567 to origin
+
+
+Both steps still require going and making the Pull Request in GitHub, merging, and deleting the new branch.  But at least it's a start.  Would be nice to have a standing open ticket to reference for this.
---
+++ bump.py
@@ -0,0 +1,91 @@
+#
+# Script for bumping versions of Android WSDK
+# 
+# When we release a build of WSDK, the release branch should have it's point release incremented 
+#
+# When we create release branches, the develop branch should have it's minor release incremented
+#
+# TODO: Should we have a ticket that just stays open for these?  
+# TODO: Is it possible to auto create a PR? requires github library and credentials
+#
+
+import git
+import os
+import shutil
+import sys
+
+from flask import Flask
+app = Flask(__name__)
+
+# FIXME just for debugging
+git_url="git@github.com:ourkov-gpsw/my-dumb-android-library.git"
+workspace="./workspace"
+debug=False
+
+def prepWorkspace(branch, ticket):
+	print "removing any existing workspace"
+	if os.path.isdir(workspace):
+		shutil.rmtree(workspace)
+	if branch != 'develop':
+		branch = "release/%s" % branch
+	print "Checking out %s branch of %s" % (branch, git_url)
+	os.system("git clone --recursive -b %s %s %s" % (branch, git_url, workspace))
+	featureBranch="feature/%s" % ticket
+        print "checking out %s" % featureBranch
+        cmd = "git -C %s  checkout -b %s" % (workspace, featureBranch)
+	print cmd
+	os.system(cmd)
+
+def bumpVersion(version_type):
+	propfile=os.path.sep.join([workspace, "wsdk", "gradle.properties"])
+        contents = open (propfile, 'r').readlines()
+        for line in contents:
+		if line.startswith("version"):
+			# split version into an array
+			version=line.split('=')[1].split('.')
+ 			print "old version: %s" % version
+			if version_type == "minor":
+ 				version[1] = str(int(version[1]) + 1)
+			else:
+ 				version[2] = str(int(version[2]) + 1)
+ 			print "new version: %s" % version
+	newVersion = '.'.join(version).strip()
+	f = open(propfile, 'w')
+ 	for line in contents:
+ 		if line.startswith("version"):
+ 			f.write("version = %s\n" % newVersion)
+ 		else:
+			f.write(line)
+	return newVersion
+
+def pushFeatureToOrigin(ticket, newVersion):
+	featureBranch="feature/%s" % ticket
+	propfile = os.path.abspath(os.path.sep.join([workspace, "wsdk", "gradle.properties"]))
+	commitMessage = "[%s] bump version to %s" % (ticket, newVersion)
+	repo = git.Repo(os.path.abspath(workspace))
+	print "git add %s" % propfile
+	repo.git.add(propfile)
+	print "git commit -m \"%s\"" % commitMessage
+	repo.git.commit(m=commitMessage)
+	print "pushing %s branch to origin" % featureBranch
+	if not debug:
+		repo.git.push("origin", featureBranch)
+
+@app.route("/bumpminor/<branch>/<ticket>")
+def bumpMinorVersion(branch, ticket):
+	print "bumping minor version on branch %s with ticket %s" % (branch, ticket)
+	prepWorkspace(branch, ticket)
+	newVersion = bumpVersion("minor")
+	pushFeatureToOrigin(ticket, newVersion)
+	return "bumped minor version to %s on new branch feature/%s.  Need to merge PR to %s" % (newVersion, ticket, branch)
+
+@app.route("/bumppoint/<branch>/<ticket>")
+def bumpPointVersion(branch, ticket):
+	print "bumping point version on branch %s with ticket %s" % (branch, ticket)
+	prepWorkspace(branch, ticket)
+	newVersion = bumpVersion("point")
+	pushFeatureToOrigin(ticket, newVersion)
+	return "bumped point version to %s on new branch feature/%s.  Need to merge PR to %s" % (newVersion, ticket, branch)
+
+app.run()
+
